a6d677c9e7588c1385765ef3627ded7b4228a985
JakeAwsd <93892085+JakeAwsd@users.noreply.github.com>
2022-04-11 19:59:46 -0500

    Scoring system and image upload systme

---
+++ public/js/multerTEST.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const multer = require("multer");
+
+const app = express();
+
+const fileStorageEngine = multer.diskStorage({
+destination: (req, file, cb) => {
+    cb(null, './images')
+},
+
+filename: (req,file,cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+},
+});
+
+const upload = multer({ storage: fileStorageEngine});
+
+app.post("/single", upload.single('avatar'), (req, res) => {
+    console.log(req.file);
+    res.send("Single FIle Upload Successful")
+});
+
+app.listen(5000);
